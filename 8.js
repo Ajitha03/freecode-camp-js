6f0c8ad1e37390f91548c147387e9bee9388171d
Ajitha03 <55685515+Ajitha03@users.noreply.github.com>
2019-10-15 09:37:42 +0530

    Add files via upload

---
+++ 8.js
@@ -0,0 +1,3 @@
+var difference = 45 - 33; 
+//make this equal to 12 by changing the 0 into the appropriate number.
+
---
+++ 9.js
@@ -0,0 +1,2 @@
+var product = 2 * 10; 
+// Make this equal to 20 by changing the 0 into the appropriate number.
